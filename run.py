5fcefef041c69d50e84f204ce9e27baf49edcee7
Bob Herold <bobherold@yahoo.com>
2020-09-09 14:12:11 +0000

    Added form code w/button in index.html and added GET, POST methods to index view in run.py

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import os
 from datetime import datetime
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "randomstring123"
 messages = []
 
 def add_messages(username, message):
@@ -15,10 +16,17 @@ def get_all_messages():
     return "<br>".join(messages)
 
 
-@app.route('/')
+@app.route('/', methods = ["Get", "POST"] )
 def index():
     """Main page with instructions"""
-    return renter_template("index.html")
+
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+
+    if "username" in session:
+        return redirect(session["username"])
+
+    return render_template("index.html")
 
 @app.route('/<username>')
 def user(username):
